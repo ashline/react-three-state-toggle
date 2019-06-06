174302243b07b03f7fa15f8ee3917366dbd04f4a
Ashline Mpofu <ashline.mpofu@webinterpret.com>
2019-06-06 18:42:12 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
-import React, { PureComponent } from "react"
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+const noop = () => {};
+const defaultValues = [
+  { label: "Yes", value: true },
+  { label: "", value: null },
+  { label: "No", value: false }
+];
 
 class ThreeStateToggle extends PureComponent {
+  static propTypes = {
+    onChange: PropTypes.func,
+    onLeft: PropTypes.func,
+    onMiddle: PropTypes.func,
+    onRight: PropTypes.func,
+    defaultValueIndex: PropTypes.oneOf([0, 1, 2]),
+    values: PropTypes.array
+  };
+  static defaultProps = {
+    onChange: noop,
+    onLeft: noop,
+    onMiddle: noop,
+    onRight: noop,
+    defaultValueIndex: 1,
+    values: defaultValues
+  };
+
+  render() {
+    const { values } = this.props;
+    return (
+      <div>
+        {values.map((option, index) => (
+          <label
+            key={`toggle-label-${index}`}
+            htmlFor={`toggle-label-${index}`}
+          >
+            <input
+              id={`toggle-label-${index}`}
+              type="radio"
+              name="someDynamicName"
+              value={option.value}
+            />
+          </label>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default ThreeStateToggle
-
+/**
+ * TODO: find better id for inputs, htmlFor might not be needed
+ */
+export default ThreeStateToggle;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+import { Welcome } from "@storybook/react/demo";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import ThreeStateToggle from "..";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("ThreeStateToggle")} />
+));
+// .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+storiesOf("ThreeStateToggle", module).add("Component", () => (
+  <ThreeStateToggle />
+));
